4781b39801169e181bb600c803f35da9dd1ab7c7
Satyam Katiyar <satyam.digitem@gmail.com>
2023-07-15 16:18:53 +0530

    y
    y

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var myName = prompt("Enter your name");
 var dud = document.getElementById("SSS");
+var du = document.getElementById("2");
 myName=myName.toLowerCase();
 console.log(myName);
 if(myName=="duddu"){
     dud.classList.add("display");
+    du.classList.add("hide");
 }
